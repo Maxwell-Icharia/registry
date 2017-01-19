512e59f14f1ee6212071444cc092ff9c3cc3bc4b
Maxwell-icharia <maxwellicharia@gmail.com>
2017-01-20 00:36:51 +0300

    Updated readme and implemented proper use of http client services

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# registry
-It keeps a record of all applicants of an institution
+# Names
+
+
+It implements a website that displays common names of individuals.
+
+When prompted, the user inputs the name and it will be searched for in the website 
+and an output of the name is common or the name is not common will be returned
--- console.py
+++ console.py
@@ -1,34 +1,15 @@
-def registry():
-	name = raw_input('What is your name ?')
-	if name != str:
-		return "Invalid input, try again! " + name
-	else:
-		number = raw_input(str('What is your mobile phone number?#eg +254736253413 '))
-		if len(number) != 13:
-			return "Invalid input, try agin! " + number
+from urllib2 import Request, urlopen, URLError
 
+name = Request('http://nameberry.com/popular_names')
 
-from AfricasTalkingGateway import AfricasTalkingGateway, AfricasTalkingGatewayException
-
-username = "MaxwellIcharia"
-apikey   = "74db14cf1d848056e31b1549c18efa2b6ad6a89b63104ab5ae14c92739b05a1b"
-
-
-to      = " number"
-
-message = "Hey there " + name + " you are now registered."
-
-gateway = AfricasTalkingGateway(username, apikey)
+g = raw_input("What is your first name? ")
 
 try:
-    
-    results = gateway.sendMessage(to, message)
-    
-    for recipient in results:
-        print 'number=%s;status=%s;messageId=%s;cost=%s' % (recipient['number'],
-                                                            recipient['status'],
-                                                            recipient['messageId'],
-                                                            recipient['cost'])
-except AfricasTalkingGatewayException, e:
-    print 'Encountered an error while sending: %s' % str(e)
-
+	response = urlopen(name)
+	common = response.read()
+	if g in common:
+		print 'The name is common'
+	else:
+		print 'The name is not common'
+except URLError, e:
+    print 'No Common name, error!:', e
